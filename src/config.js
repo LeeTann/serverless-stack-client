fc87b5235f0b6ba689374d2be56aa135054a60ca
ltann <leeroyleeroy18@gmail.com>
2020-06-13 14:46:55 -0700

    Set up aws-amplify config to connect with backend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "leetann-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://irv2uq2k12.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_SVH5tJnO9",
+    APP_CLIENT_ID: "2jpld5sf664u4o19ch87epptjm",
+    IDENTITY_POOL_ID: "us-east-1:ede05b63-667f-4446-b49b-93e2fa0d29f6"
+  }
+};
