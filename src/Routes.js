dee53fd613c1c23303ed56717c8c01d1cf988881
ltann <leeroyleeroy18@gmail.com>
2020-06-13 21:28:37 -0700

    Added Signup route

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
@@ -12,6 +13,9 @@ export default function Routes() {
       </Route>
       <Route exact path="/login">
         <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       {/* catch all unmatched routes */}
       <Route>
