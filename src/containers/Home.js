59bed6ac4f1156ba5c256830a937633e5e6a961a
ltann <leeroyleeroy18@gmail.com>
2020-06-16 22:20:23 -0700

    Updating Home Lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { API } from "aws-amplify";
 import "./Home.css";
-
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
@@ -60,6 +60,14 @@ export default function Home() {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
